b48703889e8f62de8f2030ae230f179dd80263a3
nioliu <nioliu@tencent.com>
2023-03-10 12:54:08 +0800

    first commit

---
+++ objects.go
@@ -0,0 +1,28 @@
+package util
+
+type Result struct {
+	Items []*Item `json:"items"`
+}
+
+type Item struct {
+	Uid          string            `json:"uid"`
+	Type         string            `json:"type"`
+	Title        string            `json:"title"`
+	Subtitle     string            `json:"subtitle"`
+	Arg          string            `json:"arg"` // 相当于query
+	Autocomplete string            `json:"autocomplete"`
+	Icon         *Icon             `json:"icon"`
+	Variables    map[string]string `json:"variables"`
+	QuickLookUrl string            `json:"quicklookurl"`
+	Text         *Text             `json:"text"` // Defines the text the user will get when copying the selected result row with ⌘C or displaying large type with ⌘L.
+}
+
+type Text struct {
+	Copy      string `json:"copy"`
+	Largetype string `json:"largetype"`
+}
+
+type Icon struct {
+	Type string `json:"type"`
+	Path string `json:"path"`
+}
